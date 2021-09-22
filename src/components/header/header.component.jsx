2346bd83b05889d01a8dbcf2d5cc7027672fd103
CodeSatish <mail2satishc@gmail.com>
2021-09-22 22:36:02 +0530

    styled component added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 import { createStructuredSelector } from 'reselect';
@@ -7,39 +6,41 @@ import { createStructuredSelector } from 'reselect';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
-import { selectCartHidden, selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
+
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
 import './header.styles.css';
 
 const Header = ({currentUser, hidden}) => (
-    <div className='header'>
-        <Link className='logo-container' to="/">
+    <HeaderContainer>
+        <LogoContainer to="/">
             <Logo className='logo'/>
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink  to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 CONTACT
-            </Link>
+            </OptionLink>
             {
                 currentUser ? (
-                    <div className='option' onClick={()=> auth.signOut()} >
+                    <OptionLink as='div' onClick={()=> auth.signOut()} >
                         
                         SIGN OUT
-                    </div>
+                    </OptionLink>
                  ) : (
-                     <Link className='option' to='/signin'>
+                     <OptionLink to='/signin'>
                          SIGN IN
-                    </Link>
+                    </OptionLink>
                     )
             }
             <CartIcon></CartIcon>
-        </div>
+        </OptionsContainer>
         {hidden ? null : <CartDropDown />}
-    </div>
+    </HeaderContainer>
 )
 
 const mapStateToProps= createStructuredSelector({
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.components';
 
-import './homepage.styles.css';
+import {HomePageContainer} from './homepage.styles';
 
 const HomePage = () => (
-    <div className='homepage'>
+    <HomePageContainer>
         <Directory />
-    </div>
+    </HomePageContainer>
 
 
 );
